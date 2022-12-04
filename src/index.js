766604deea0f39efab11bbc5e883c6f7dc6b3b41
euzzell <119771932+euzzell@users.noreply.github.com>
2022-12-03 22:51:28 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+reportWebVitals();
